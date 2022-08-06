130c50eb7afaf9d05ea86c3cbe117ba2acc15292
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-08-06 19:35:41 +0800

    Added Thief

---
+++ units/chicken_thief.lua
@@ -0,0 +1,109 @@
+return { chicken_thief = {
+  unitname            = [[chicken_thief]],
+  name                = [[Dummy Thief]],
+  description         = [[Basic Assassin]],
+  acceleration        = 1.08,
+  activateWhenBuilt   = true,
+  brakeRate           = 1.23,
+  buildCostEnergy     = 0,
+  buildCostMetal      = 100,
+  builder             = false,
+  buildPic            = [[thief.png]],
+  buildTime           = 100,
+  canGuard            = true,
+  canMove             = true,
+  canPatrol           = true,
+  canCloak            = true,
+  category            = [[LAND SINK SMALL]],
+  collisionVolumeScales  = [[27 42 27]],
+  collisionVolumeType    = [[cylY]],
+  usePieceSelectionVolumes = true,
+
+  customParams        = {
+    outline_x = 70,
+    outline_y = 70,
+    outline_yoff = 15,
+  },
+
+  explodeAs           = [[NOWEAPON]],
+  footprintX          = 2,
+  footprintZ          = 2,
+  iconType            = [[chicken]],
+  idleAutoHeal        = 20,
+  idleTime            = 300,
+  maxDamage           = 400,
+  maxSlope            = 36,
+  maxVelocity         = 3.3,
+  movementClass       = [[AKBOT2]],
+  noAutoFire          = false,
+  noChaseCategory     = [[SHIP FLOAT SWIM TERRAFORM FIXEDWING SATELLITE GUNSHIP]],
+  objectName          = [[thief.dae]],
+  script              = [[chicken_thief.lua]],
+  power               = 100,
+  reclaimable         = false,
+  selfDestructAs      = [[NOWEAPON]],
+
+  sfxtypes            = {
+
+    explosiongenerators = {
+      [[custom:blood_spray]],
+      [[custom:blood_explode]],
+      [[custom:dirt]],
+    },
+
+  },
+  sightDistance       = 512,
+  sonarDistance       = 512,
+  turnRate            = 967,
+  upright             = true,
+  workerTime          = 0,
+
+  weapons             = {
+
+    {
+      def                = [[WEAPON]],
+      mainDir            = [[0 0 1]],
+      maxAngleDif        = 120,
+      onlyTargetCategory = [[SWIM LAND SUB SINK TURRET FLOAT SHIP HOVER FIXEDWING GUNSHIP]],
+    },
+
+  },
+
+
+  weaponDefs          = {
+
+    WEAPON = {
+      name                    = [[Thief Slash]],
+      avoidFeature            = false,
+      avoidFriendly           = false,
+      collideFeature          = false,
+      collideFriendly         = false,
+      craterBoost             = 0,
+      craterMult              = 0,
+	  turret                  = true,
+
+      damage                  = {
+        default = 100,
+        planes  = 100,
+      },
+
+      explosionGenerator      = [[custom:NONE]],
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 0,
+      range                   = 75,
+      reloadtime              = 0.4,
+      size                    = 0,
+      soundStart              = [[dummy/swordswing]],
+      targetborder            = 1,
+      tolerance               = 5000,
+      turret                  = true,
+      waterWeapon             = true,
+      weaponType              = [[Cannon]],
+      weaponVelocity          = 500,
+    },
+
+  },
+
+} }
