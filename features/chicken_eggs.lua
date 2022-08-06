ce91348f22fd6a7c69e5e294d6cb73aff20eb863
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-08-06 19:36:26 +0800

    Added Thief studs

--- features/chicken_eggs.lua
+++ features/chicken_eggs.lua
@@ -74,6 +74,12 @@ eggDefs.chicken_defender_egg = MergeTable(defaultEgg, {
   object      = [[stud2.dae]],
 })
 
+eggDefs.chicken_thief_egg = MergeTable(defaultEgg, {
+  metal       = 40,
+  reclaimTime = 40,
+  object      = [[stud2.dae]],
+})
+
 -- specify origin unit (for tooltip/contextmenu)
 for name,data in pairs(eggDefs) do
 	local unitname = name
