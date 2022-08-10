41f233e744472ce217dc4e26322ca732de7629a2
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-08-11 01:40:10 +0800

    Dauntless Spirit

--- effects/sphere.lua
+++ effects/sphere.lua
@@ -7,11 +7,37 @@ return {
             count              = 1,
             ground             = true,
             water              = true,
-            underwater          = true,
+            underwater         = true,
             properties = {
                 alpha              = 0.9, -- how transparent it is (1 is not, 0 is transparent)
                 color              = [[0.3, 0.1, 0.8]], -- change color here. RGBA
                 expansionspeed     = 5, -- how fast it expands
+                ttl                = 10, -- how long it lives
+            },
+        },
+    },
+	["dauntless_spirit"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 1,
+            circlegrowth       = 40,
+            flashalpha         = 1,
+            flashsize          = 10,
+            ttl                = 10,
+            color              = [[0.0, 0.5, 0.8]],
+    },
+        sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.0, 0.5, 0.8]], -- change color here. RGBA
+                expansionspeed     = 20, -- how fast it expands
                 ttl                = 10, -- how long it lives
             },
         },
