b698d0167b1930c3629fd03154b93754c2bbdb1a
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-08-30 20:17:35 +0800

    grenade_launcher and gunpowder_blast

--- effects/sphere.lua
+++ effects/sphere.lua
@@ -80,4 +80,38 @@ return {
             },
         },
     },
+	["grenade_launcher"] = {
+        usedefaultexplosions = false,
+        sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 0.9, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.3, 0.1, 0.8]], -- change color here. RGBA
+                expansionspeed     = 15, -- how fast it expands
+                ttl                = 10, -- how long it lives
+            },
+        },
+    },
+	["gunpowder_blast"] = {
+        usedefaultexplosions = false,
+        sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 0.9, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.3, 0.1, 0.8]], -- change color here. RGBA
+                expansionspeed     = 7.5, -- how fast it expands
+                ttl                = 10, -- how long it lives
+            },
+        },
+    },
 }
