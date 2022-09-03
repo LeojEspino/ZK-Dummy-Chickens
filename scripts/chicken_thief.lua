1c973370257c108cb2d124f581f0451ef8816d3a
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-09-04 00:29:06 +0800

    Add files via upload

--- scripts/chicken_thief.lua
+++ scripts/chicken_thief.lua
@@ -99,10 +99,6 @@ function script.StopMoving()
 	StartThread(StopWalk)
 end
 
-function script.Create()
-	Spring.SetUnitCloak(unitID, 2)	--free cloak
-end
-
 local function RestoreAfterDelay()
 	Sleep(1000)
 	aiming = false
