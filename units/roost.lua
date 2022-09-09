79af6ccc593bff5042951b45c803d6556860936f
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-09-10 02:49:23 +0800

    Added the Spawner

---
+++ units/roost.lua
@@ -0,0 +1,41 @@
+return { roost = {
+  unitname          = [[roost]],
+  name              = [[Spawner]],
+  description       = [[Spawns Dummies]],
+  activateWhenBuilt = true,
+  buildCostMetal    = 700,
+  builder           = false,
+  buildPic          = [[spawner.png]],
+  category          = [[SINK]],
+  useCollisionSelectionVolumes = true,
+  usePieceSelectionVolumes = true,
+
+  customParams      = {
+  },
+  
+  energyMake        = 5,
+  explodeAs         = [[NOWEAPON]],
+  footprintX        = 4,
+  footprintZ        = 4,
+  iconType          = [[special]],
+  idleAutoHeal      = 20,
+  idleTime          = 300,
+  levelGround       = false,
+  maxDamage         = 4000,
+  maxSlope          = 36,
+  maxVelocity       = 0,
+  metalMake         = 2.5,
+  noAutoFire        = false,
+  objectName        = [[spawner.dae]],
+  script            = [[roost.lua]],
+  selfDestructAs    = [[NOWEAPON]],
+  sightDistance     = 512,
+  upright           = false,
+  waterline         = 0,
+  workerTime        = 0,
+  yardMap           = [[yyyyyyyyy]],
+
+  featureDefs       = {
+  },
+
+} }
