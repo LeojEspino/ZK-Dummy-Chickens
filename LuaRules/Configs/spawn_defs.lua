e5c56d316f6930c9c9c295ad93332438887a7268
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-10-11 23:47:13 +0800

    Added Dummy Faraday to spawner

--- LuaRules/Configs/spawn_defs.lua
+++ LuaRules/Configs/spawn_defs.lua
@@ -173,6 +173,7 @@ local defenders = Spring.Utilities.CustomKeyToUsefulTable(Spring.GetModOptions()
   chicken_lotus = {time = 6, squadSize = 1, cost = 1 },
   chicken_picket = {time = 6, squadSize = 1, cost = 1 },
   chicken_stardust = {time = 12, squadSize = 1, cost = 2 },
+  chicken_faraday = {time = 12, squadSize = 1, cost = 2 },
 }
 
 local supporters = Spring.Utilities.CustomKeyToUsefulTable(Spring.GetModOptions().campaign_chicken_types_support) or {
