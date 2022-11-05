794d40aea5630f280ae7e138cf7de1cb984cc3c4
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-11-05 17:54:05 +0800

    Added Dummy Hacksaw studs

--- features/chicken_eggs.lua
+++ features/chicken_eggs.lua
@@ -82,6 +82,12 @@ eggDefs.chicken_razor_egg = MergeTable(defaultEgg, {
   object      = [[stud3.dae]],
 })
 
+eggDefs.chicken_hacksaw_egg = MergeTable(defaultEgg, {
+  metal       = 80,
+  reclaimTime = 80,
+  object      = [[stud3.dae]],
+})
+
 --The Dummy
 
 eggDefs.chicken_egg = MergeTable(defaultEgg, {
