7362fef12d4d8045f34945c6958e746e684a25ef
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-12-11 21:03:17 +0800

    Added Dummy Artemis studs

--- features/chicken_eggs.lua
+++ features/chicken_eggs.lua
@@ -124,6 +124,12 @@ eggDefs.chicken_chainsaw_egg = MergeTable(defaultEgg, {
   object      = [[stud5.dae]],
 })
 
+eggDefs.chicken_artemis_egg = MergeTable(defaultEgg, {
+  metal       = 720,
+  reclaimTime = 720,
+  object      = [[stud5.dae]],
+})
+
 --The Dummy
 
 eggDefs.chicken_egg = MergeTable(defaultEgg, {
