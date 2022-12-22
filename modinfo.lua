1ae3f8c0844645fcac0e53536335393fdc3fe7a7
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-12-22 19:45:16 +0800

    v0.7.9

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = 'Dummy Invaders',
 	description = 'Replaces Chickens with Dummies',
 	shortname = 'ZKDI',
-	version = 'v0.7.8.1',
+	version = 'v0.7.9',
 	mutator = '1',
 	game = 'Zero-K',
 	shortGame = 'ZK',
