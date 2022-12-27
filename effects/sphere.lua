8dfdc0f33fef7826c7ae6a3b1ae31405c8615be2
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-12-28 03:54:21 +0800

    Added Dummy Bomber's effects

--- effects/sphere.lua
+++ effects/sphere.lua
@@ -266,4 +266,47 @@ return {
             color              = [[1.0, 1.0, 1.0]],
         },
     },
+	["bouncing_bomb"] = {
+        usedefaultexplosions = false,
+        sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 0.9, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.3, 0.1, 0.8]], -- change color here. RGBA
+                expansionspeed     = 10, -- how fast it expands
+                ttl                = 10, -- how long it lives
+            },
+        },
+    },
+	["cross_explosion"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 1,
+            circlegrowth       = 40,
+            flashalpha         = 1,
+            flashsize          = 10,
+            ttl                = 10,
+            color              = [[0.0, 0.5, 0.8]],
+        },
+        sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.3, 0.1, 0.8]], -- change color here. RGBA
+                expansionspeed     = 10, -- how fast it expands
+                ttl                = 30, -- how long it lives
+            },
+        },
+    },
 }
