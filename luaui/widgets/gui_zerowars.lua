d863fb15e671a5c025fc6f89d07339fdee663fcf
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-12-30 05:11:28 +0800

    Add files via upload

---
+++ luaui/widgets/gui_zerowars.lua
@@ -0,0 +1,13 @@
+function widget:GetInfo()
+    return {
+        name = "Zero Wars GUI",
+        desc = "zero-wars local gui",
+        author = "petturtle",
+        date = "2020",
+        license = "GNU GPL, v2 or later",
+        layer = 0,
+        enabled = true
+    }
+end
+
+function widget:Initialize() end
