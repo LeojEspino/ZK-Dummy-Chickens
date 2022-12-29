aa652bfd033a90d1d4651c80bebab4921223cf7f
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2022-12-30 05:08:49 +0800

    Delete scripts directory

--- scripts/roost.lua
+++
@@ -1,6 +0,0 @@
-local spawner = piece 'spawner'
--- the rim is piece 'base' but not actually used
-
-function script.AimWeapon() return true end
-function script.AimFromWeapon() return spawner end
-function script.QueryWeapon() return spawner end
