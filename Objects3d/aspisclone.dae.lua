6ed93f843a7a39c8e3d2560f3460e53de742bbbe
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2023-03-21 23:27:25 +0800

    Added Dummy Aspis and Dummy Aegis' model

---
+++ Objects3d/aspisclone.dae.lua
@@ -0,0 +1,5 @@
+return {
+	tex1 = "dummy1.dds",
+	tex2 = "dummy2.dds",
+	midpos = {0, 12, 0},
+}
---
+++ Objects3d/aspisclone_team.dae.lua
@@ -0,0 +1,5 @@
+return {
+	tex1 = "dummy1_team.dds",
+	tex2 = "dummy2.dds",
+	midpos = {0, 12, 0},
+}
