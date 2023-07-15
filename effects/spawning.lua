696791756fde93ff4d200b101a5a7db407643706
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2023-07-16 04:35:28 +0800

    spawning effect

---
+++ effects/spawning.lua
@@ -0,0 +1,418 @@
+return {
+    ["spawning1"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 10,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 10,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[1.0, 1.0, 1.0]], -- change color here. RGBA
+                expansionspeed     = 5, -- how fast it expands
+                ttl                = 10, -- how long it lives
+            },
+        },
+    },
+	["spawning2_brawler"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 15,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 15,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.8, 0.5, 0.2]], -- change color here. RGBA
+                expansionspeed     = 7.5, -- how fast it expands
+                ttl                = 15, -- how long it lives
+            },
+        },
+    },
+	["spawning2_marksman"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 15,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 15,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.0, 0.5, 0.8]], -- change color here. RGBA
+                expansionspeed     = 7.5, -- how fast it expands
+                ttl                = 15, -- how long it lives
+            },
+        },
+    },
+	["spawning2_assassin"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 15,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 15,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[1.0, 0.0, 0.0]], -- change color here. RGBA
+                expansionspeed     = 7.5, -- how fast it expands
+                ttl                = 15, -- how long it lives
+            },
+        },
+    },
+	["spawning2_burst"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 15,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 15,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[1.0, 0.0, 1.0]], -- change color here. RGBA
+                expansionspeed     = 7.5, -- how fast it expands
+                ttl                = 15, -- how long it lives
+            },
+        },
+    },
+	["spawning2_tank"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 15,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 15,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.0, 1.0, 0.0]], -- change color here. RGBA
+                expansionspeed     = 7.5, -- how fast it expands
+                ttl                = 15, -- how long it lives
+            },
+        },
+    },
+	["spawning3_brawler"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 20,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 20,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.8, 0.5, 0.2]], -- change color here. RGBA
+                expansionspeed     = 10, -- how fast it expands
+                ttl                = 20, -- how long it lives
+            },
+        },
+    },
+	["spawning3_marksman"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 20,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 20,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.0, 0.5, 0.8]], -- change color here. RGBA
+                expansionspeed     = 10, -- how fast it expands
+                ttl                = 20, -- how long it lives
+            },
+        },
+    },
+	["spawning3_assassin"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 20,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 20,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[1.0, 0.0, 0.0]], -- change color here. RGBA
+                expansionspeed     = 10, -- how fast it expands
+                ttl                = 20, -- how long it lives
+            },
+        },
+    },
+	["spawning3_burst"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 20,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 20,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[1.0, 0.0, 1.0]], -- change color here. RGBA
+                expansionspeed     = 10, -- how fast it expands
+                ttl                = 20, -- how long it lives
+            },
+        },
+    },
+	["spawning3_tank"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 20,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 20,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.0, 1.0, 0.0]], -- change color here. RGBA
+                expansionspeed     = 10, -- how fast it expands
+                ttl                = 20, -- how long it lives
+            },
+        },
+    },
+	["spawning4_brawler"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 25,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 25,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.8, 0.5, 0.2]], -- change color here. RGBA
+                expansionspeed     = 12.5, -- how fast it expands
+                ttl                = 25, -- how long it lives
+            },
+        },
+    },
+	["spawning4_marksman"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 25,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 25,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.0, 0.5, 0.8]], -- change color here. RGBA
+                expansionspeed     = 12.5, -- how fast it expands
+                ttl                = 25, -- how long it lives
+            },
+        },
+    },
+	["spawning4_assassin"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 25,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 25,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[1.0, 0.0, 0.0]], -- change color here. RGBA
+                expansionspeed     = 12.5, -- how fast it expands
+                ttl                = 25, -- how long it lives
+            },
+        },
+    },
+	["spawning4_burst"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 25,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 25,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[1.0, 0.0, 1.0]], -- change color here. RGBA
+                expansionspeed     = 12.5, -- how fast it expands
+                ttl                = 25, -- how long it lives
+            },
+        },
+    },
+	["spawning4_tank"] = {
+        usedefaultexplosions = false,
+		groundflash = {
+            alwaysvisible      = true,
+            circlealpha        = 0.5,
+            circlegrowth       = 25,
+            flashalpha         = 0.5,
+            flashsize          = 5,
+            ttl                = 25,
+            color              = [[1.0, 1.0, 1.0]],
+        },
+		    sphere = {
+            air                = true,
+            class              = [[CSpherePartSpawner]],
+            count              = 1,
+            ground             = true,
+            water              = true,
+            underwater         = true,
+            properties = {
+                alpha              = 1, -- how transparent it is (1 is not, 0 is transparent)
+                color              = [[0.0, 1.0, 0.0]], -- change color here. RGBA
+                expansionspeed     = 12.5, -- how fast it expands
+                ttl                = 25, -- how long it lives
+            },
+        },
+    },
+}
